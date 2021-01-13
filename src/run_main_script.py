d644c5c66908d30b98956e0d2a0a0abea469ccf4
azhar0100 <azhartahir0100@gmail.com>
2021-01-13 07:29:58 +0500

    Testing with 150 epochs again

--- src/run_main_script.py
+++ src/run_main_script.py
@@ -14,5 +14,5 @@ try:
 except:
 	pass
 
-command_str = "mnist mnist_LeNet ../log/mnist_test ../data --objective one-class --lr 0.0001 --n_epochs 1 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 1 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 3 --retrain_decoder"
+command_str = "mnist mnist_LeNet ../log/mnist_test ../data --objective one-class --lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 3 --retrain_decoder"
 main(command_str.split())
