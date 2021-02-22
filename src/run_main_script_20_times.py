f68c067a3599d36d1357d2122b3216c7eba70b3b
azhar0100 <azhartahir0100@gmail.com>
2021-02-23 04:04:57 +0500

    A new approach

--- src/run_main_script_20_times.py
+++ src/run_main_script_20_times.py
@@ -17,10 +17,12 @@ try:
 except:
 	pass
 
-command_str = "toydataset toynet ../log/test{0}/ ../data --objective one-class --lr 0.0001 --n_epochs 1000 --lr_milestone 50 --batch_size 150 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 1000 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0 --retrain_decoder"
+import pathlib
+command_str = "toydataset toynet ../log/test{0}/ ../data --objective one-class --lr 0.0001 --n_epochs 10 --lr_milestone 50 --batch_size 150 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 10 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0 --retrain_decoder"
+
 for i in range(20):
-	try:
-		sh.mkdir("../log/test{}".format(str(i).zfill(2)))
-	except:
-		pass
-	main(command_str.format(str(i).zfill(2)).split())
+	pathlib.Path('../log/test{}'.format(str(i).zfill(2))).mkdir(parents=True, exist_ok=True)
+	# for j in range(10):
+		# print(i)
+	# main(command_str.format(str(i).zfill(2)).split())
+	print("!python src/main.py {}".format(command_str.format(str(i).zfill(2))))
