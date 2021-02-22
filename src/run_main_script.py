e6df37032e5afa77d904b3ffac91b2ad2583ad37
azhar0100 <azhartahir0100@gmail.com>
2021-02-23 00:34:57 +0500

    Tweaked parameters

--- src/run_main_script.py
+++ src/run_main_script.py
@@ -14,5 +14,5 @@ try:
 except:
 	pass
 
-command_str = "toydataset toynet ../log/mnist_test ../data --objective one-class --lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 150 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0 --retrain_decoder"
+command_str = "toydataset toynet ../log/mnist_test ../data --objective one-class --lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 150 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0"
 main(command_str.split())
