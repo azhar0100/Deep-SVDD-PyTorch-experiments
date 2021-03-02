e3d464fd933ffcf562d3afb5a98451b2c9ad9f76
Tatheer Zahra <TatheerZahara@users.noreply.github.com>
2021-03-02 19:38:47 +0500

    Update run_main_script.py

--- src/run_main_script.py
+++ src/run_main_script.py
@@ -16,3 +16,8 @@ except:
 
 command_str = "main.py toydataset toynet ../log/mnist_test ../data --objective one-class --lr 0.0001 --n_epochs 1000 --lr_milestone 50 --batch_size 150 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 1000 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0 --retrain_decoder"
 main(command_str.split())
+
+# Tatheer's glorious code addition
+# So much code
+# So many AI
+
